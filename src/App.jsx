75c2497ec98b1c66907530f5eea8d35a593af7c1
Tyson Power <tysonpower@gmail.com>
2017-01-30 23:19:58 -0800

    add api init

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,15 @@ import React, {Component} from 'react';
 class App extends Component {
   render() {
     return (
-      <h1>Hello React2 :)</h1>
+    	<section>
+	      <h1>Hello React2 :)</h1>
+	    	<button
+				class="g-recaptcha"
+				data-sitekey="6LcTyhMUAAAAADeoa78twejqnSKWf5bbhZMT377z"
+				data-callback="YourOnSubmitFn(response)">
+				Submit
+			</button>
+		</section>
     );
   }
 }
